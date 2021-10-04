91098f501e3e4adadd34d7b2f318fe4f452b3076
Dzianis <dzianisyaukhuta@gmail.com>
2021-10-04 09:52:13 +0300

    Time: 54:00

---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import listSvg from '../assets/img/list.svg';
+
+const List = () => {
+    return (
+        <ul className="todo__list">
+            <li>
+                <i>
+                    <img src={listSvg} alt="" />
+                </i>
+                <span>Все задачи</span>
+            </li>
+        </ul>
+    );
+}
+
+export default List;
