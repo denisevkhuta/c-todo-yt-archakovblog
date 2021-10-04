45a305d3ec06f0a4da0750388d7d19639c40c466
Dzianis Yaukhuta <dzianisyaukhuta@gmail.com>
2021-10-04 10:46:43 +0300

    L2, Time: 2:40

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import List from './components/List';
+import List from './components/List/';
 
 function App() {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-        <List items="Hello" />
+        <List items={[
+          {
+            color: 'green',
+            name: 'Все задачи',
+            active: true
+          }
+        ]} />
+        <List items={[
+          {
+            color: 'green',
+            name: 'Покупки'
+          },
+          {
+            color: 'blue',
+            name: 'Фронтенд'
+          },
+          {
+            color: 'pink',
+            name: 'Фильмы и сериалы'
+          },
+        ]} />
       </div>
       <div className="todo__tasks">
 
