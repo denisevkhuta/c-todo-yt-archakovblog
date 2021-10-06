d9f08f857052ea75a3f9ef90d634c96a5113e7ba
Dzianis <dzianisyaukhuta@gmail.com>
2021-10-06 21:17:16 +0300

    L3 00:00

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import Badge from '../Badge';
 import List from '../List'
 
+import closeSvg from '../../assets/img/close.svg'
+
 import './AddList.scss'
 
 const AddList = ({ colors }) => {
 
-    const [visiblePopup, setVisiblePopup] = useState(true);
-    
+    const [visiblePopup, setVisiblePopup] = useState(false);
+    const [selectedColor, selectColor] = useState(colors[0].id);
 
     return (
         <div className="add-list">
@@ -20,17 +22,25 @@ const AddList = ({ colors }) => {
                     }
                 ]} />
             {visiblePopup && <div className="add-list__popup">
+                <img 
+                onClick={() => setVisiblePopup(false)}
+                src={closeSvg} alt="" className="add-list__popup-close-btn" />
                 <input className="field" type="text" placeholder="Название списка" />
                 <div className="add-list__popup-colors">
                     {
                         colors.map(
                             color => (
-                                <Badge onClick={() => alert(color.id)} key={color.id} color={color.name} />
+                                <Badge 
+                                onClick={() => selectColor(color.id)} 
+                                key={color.id} 
+                                color={color.name} 
+                                className={selectedColor === color.id && 'active'}
+                                />
                             )
                         )
                     }
                 </div>
-                <button>Добавить</button>
+                <button className="button">Добавить</button>
             </div>}
         </div>
     )
--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import './Badge.scss'
 
-const Badge = ( {color, onClick} ) => {
+const Badge = ( {color, onClick, className} ) => {
     return (
         <div>
-            <i onClick={onClick} className={`badge badge--${color}`}></i>
+            <i onClick={onClick} className={`badge badge--${color} ${className}`}></i>
         </div>
     )
 }
